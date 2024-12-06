29bd2408b5b85dea81ef85653b33cb5d97017d6e
Adnan <adnandesign47@gmail.com>
2024-12-06 15:16:23 +0500

    final project update

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# dynamic-bg-color
-A simple web app that allows users to change the background color of the page by clicking on different button.
-It used basic DOM manipulation and event handling in JavaScript.
-User click on the button to change the background color of the page.
+# Color Mood Changer
+
+**Color Modd Changer** is an Interactive web app that allow users to change the background color of the
+webpage by simple clicking on one of the color. When the color is change, the app provide information
+about the selected color.
+"I had a great experience applying hands-on practice with DOM manipulation, such as querying elements, handling events, and updating content and styles based on user interaction."
+
+## Feature
+
+- Click on one of the color blocks to change the background color of the page.
+- After clicking a color, the app will display a title and a brief description of that color's meaning.
+- A simple and easy-to-use interface, with no additional setup required.
+
+## Technologies Used
+
+- **HTML**: For structuring the webpage.
+- **CSS**: For styling and layout.
+- **JavaScript**: For adding interactivity, handling color changes, and updating content dynamically.
---
+++ app.js
@@ -0,0 +1,34 @@
+let colors = document.querySelectorAll('.color');
+let page = document.querySelector('body');
+let clrHeading = document.querySelector('#clrHeading');
+let clrInfo = document.querySelector('#clrInfo');
+
+for (let color of colors){
+    color.addEventListener('click', function(){
+        clrHeading.innerText = "";
+        clrInfo.innerHTML = "";
+        clrInfo.style.display = 'block';
+        console.log(color.id);
+        page.style.backgroundColor = color.id;
+
+        switch(color.id){
+            case "grey":
+                clrHeading.innerText = "Grey";
+                clrInfo.innerHTML = "<strong>Grey</strong> is often associated with neutrality, balance, and calmness. It can represent a sense of sophistication and maturity, or sometimes detachment."
+                break;
+
+            case "yellow":
+                clrHeading.innerText = "Yellow";
+                clrInfo.innerHTML = "<strong>Yellow</strong> is typically linked with positivity, happiness, and energy. It is often associated with creativity, optimism, and a sense of cheerfulness."
+                break;
+            case "green":
+                    clrHeading.innerText = "Green";
+                    clrInfo.innerHTML = "<strong>Green</strong> symbolizes nature, growth, and tranquility. It is often seen as a calming color that represents health, balance, and renewal."
+                    break;
+            case "blue":
+                    clrHeading.innerText = "Blue";
+                    clrInfo.innerHTML = "<strong>Blue</strong> is associated with calmness, stability, and trust. It represents wisdom, serenity, and reliability, often evoking a sense of peace and harmony."
+                    break;
+        }
+    })
+}
